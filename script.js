64a3a99de84a904e6a7e8de58b0eecd8b3006921
Dhruv Soni <57015525+dhruvsoni-2001@users.noreply.github.com>
2022-12-10 11:20:45 +0000

    exp1

---
+++ script.js
@@ -0,0 +1,53 @@
+
+import {airports} from "./data/indianAirports.js";
+let travelerCount = {
+	adult_count: 0,
+	
+};
+
+console.log(airports);
+
+console.log("Hello World!");
+
+const fromCity = document.querySelector("#from_city");
+const toCity = document.querySelector("#to_city");
+
+const form = document.querySelector("#booking_form");
+const counterButton = document.querySelector(".counterButton");
+
+counterButtons.forEach((cur)=>{
+
+});
+
+
+const handleFormSubmit("submit", (e)=> {
+	e.preventDefault();
+	const form =e.target;
+	const fromCity = form.from_city.value;
+	const toCity = form.to_city.value;
+	const departure = form.departure.value;
+	const 
+});
+
+
+form.addEventListener("submit", (e)=> {
+	e.preventDefault();
+})
+
+const fromCityOptions = airports.map((cur)=> {
+	const option = document.createElement("option");
+	option.value = cur.IATA_code;
+	option.innerText = cur.airport_name;
+	return option;
+});
+
+locationOptions.map((cur)=>{
+	parentNode.appendChild(cur);
+});
+
+applyLocationOption(fromCity);
+applyLocationOption(toCity);
+
+console.log(a);
+
+var 
